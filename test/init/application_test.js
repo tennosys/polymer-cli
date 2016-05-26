b7e6793b169712f587225549aa063c06232ab49c
Fred K. Schott <fschott@google.com>
2016-05-26 10:18:43 -0700

    add tests for new copyTpl format

---
+++ test/init/application_test.js
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+ * This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+ * The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+ * The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+ * Code distributed by Google as part of the polymer project is also
+ * subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+ */
+
+'use strict';
+
+const assert = require('chai').assert;
+const fs = require('fs');
+const path = require('path');
+const yoAssert = require('yeoman-assert');
+const helpers = require('yeoman-test');
+const ApplicationGenerator = require('../../lib/init/application/application.js').ApplicationGenerator;
+
+suite('init/application', () => {
+
+  test('creates expected files while ignoring filenames with dangling underscores', (done) => {
+
+    helpers
+      .run(ApplicationGenerator)
+      .on('end', (a) => {
+        yoAssert.file(['bower.json']);
+        yoAssert.noFile(['src/_element/_element.html']);
+        done();
+      });
+
+  });
+
+});
